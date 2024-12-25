bad3feca7abcc7498128126c10a0c19c79791c2e
Thorsten Deinert <thorsten.deinert@udo.edu>
2024-12-25 16:37:38 +0100

    (P3.1.1) Extract method handleInput (R3.5.1) If only at the start

--- index.ts
+++ index.ts
@@ -112,15 +112,19 @@ function updateTile(y: number, x: number) {
 function handleInputs() {
   while (inputs.length > 0) {
     let current = inputs.pop();
-    if (current === Input.LEFT)
-      moveHorizontal(-1);
-    else if (current === Input.RIGHT)
-      moveHorizontal(1);
-    else if (current === Input.UP)
-      moveVertical(-1);
-    else if (current === Input.DOWN)
-      moveVertical(1);
+    handleInput(current);
   }
+}
+
+function handleInput(input: Input) {
+  if (input === Input.LEFT)
+    moveHorizontal(-1);
+  else if (input === Input.RIGHT)
+    moveHorizontal(1);
+  else if (input === Input.UP)
+    moveVertical(-1);
+  else if (input === Input.DOWN)
+    moveVertical(1);
 }
 
 function draw() {
