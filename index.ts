8a8b16a9b1913da60af475fda94aa664551cda46
Thorsten Deinert <thorsten.deinert@udo.edu>
2024-12-26 14:08:56 +0100

    P(4.3.1)  Introduce new abstraction for Tile

--- index.ts
+++ index.ts
@@ -14,6 +14,23 @@ enum Tile {
   KEY2, LOCK2
 }
 
+abstract class Tile2 {
+  isAir(): boolean { return false; }
+  isFlux(): boolean { return false; }
+  isUnbreakable(): boolean { return false; }
+  isPlayer(): boolean { return false; }
+  isStone(): boolean { return false; }
+  isFallingStone(): boolean { return false; }
+  isBox(): boolean { return false; }
+  isFallingBox(): boolean { return false; }
+  isKey1(): boolean { return false; }
+  isKey2(): boolean { return false; }
+  isLock1(): boolean { return false; }
+  isLock2(): boolean {return false; }
+}
+
+
+
 enum RawInput {
   UP, DOWN, LEFT, RIGHT
 }
