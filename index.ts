f8e72af5e5851d0bdcee9992980a208740fb1f3c
Thorsten Deinert <thorsten.deinert@udo.edu>
2024-12-26 12:54:26 +0100

    (P4.1.5) 4. Replace body with new interface method

--- index.ts
+++ index.ts
@@ -170,14 +170,7 @@ function handleInputs() {
 }
 
 function handleInput(input: Input) {
-  if (input.isLeft())
-    moveHorizontal(-1);
-  else if (input.isRight())
-    moveHorizontal(1);
-  else if (input.isUp())
-    moveVertical(-1);
-  else if (input.isDown())
-    moveVertical(1);
+  input.handle();
 }
 
 function draw() {
