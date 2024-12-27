169a0ecb02407a5d6a313b257bf314ea89a7da7b
Thorsten Deinert <thorsten.deinert@udo.edu>
2024-12-27 16:01:33 +0100

    Set this.falling outside if

--- index.ts
+++ index.ts
@@ -32,13 +32,13 @@ class FallStrategy {
   getFalling() { return this.falling; }
 
   update(tile: Tile, x: number, y: number): void {
-    if (map[y + 1][x].isAir()) {
-      this.falling = new Falling();
+    this.falling = map[y + 1][x].isAir() 
+      ? new Falling()
+      : new Resting();
+    if (this.falling.isFalling()) {
       map[y+1][x] = tile;
       map[y][x] = new Air();  
-    } else {
-      this.falling = new Resting();
-    }
+    }    
   }
 }
 
