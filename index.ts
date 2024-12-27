6a378751623b4d70a8ccb6eaa5655b5d7da59106
Thorsten Deinert <thorsten.deinert@udo.edu>
2024-12-27 15:02:48 +0100

    P(5.2.1) Combine ifs in updateTile

--- index.ts
+++ index.ts
@@ -304,16 +304,12 @@ function updateMap() {
 }
 
 function updateTile(y: number, x: number) {
-  if ((map[y][x].isStony())
-    && map[y + 1][x].isAir()) {
+  if ( map[y][x].isStony() && map[y + 1][x].isAir()
+    || map[y][x].isBoxy() && map[y + 1][x].isAir())
+ {
     map[y][x].drop();
     map[y+1][x] = map[y][x]
-    map[y][x] = new Air();
-  } else if ((map[y][x].isBoxy())
-    && map[y + 1][x].isAir()) {
-    map[y][x].drop();
-    map[y+1][x] = map[y][x]
-    map[y][x] = new Air();
+    map[y][x] = new Air();  
   } else if (map[y][x].isFalling()) {
     map[y][x].rest();
   }
